f4b76742d37e3969e35b33a6de923a0965e7771e
elizebethsujitha <165899688+elizebethsujitha@users.noreply.github.com>
2024-04-26 15:36:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,5 +14,10 @@ def about():
 def contact():
     return render_template('contact.html')
 
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
 if __name__=='__main__':
     app.run(debug=True)
